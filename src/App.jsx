2454c115c68fca7ce3482e264515733d668e21fe
OtaraAlex <mbechealex61@gmail.com>
2025-06-26 12:34:50 +0300

    add navigation bar and constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Navbar from "./components/Navbar";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl font-bold">Welcome to GSAP with React!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
